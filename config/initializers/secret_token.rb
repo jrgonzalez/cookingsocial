1ddac5a46fc35351db0251a8e4807900c5b88207
Juan Ramon Gonzalez <jgonzalez@timbasoftware.com>
2013-02-14 10:24:34 -0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cookingsocial::Application.config.secret_token = '627fcbf0ad7319a2ba9ac8dcb8527b26a9717a1d2f55ee92f1c3daeb6bcd4074ba006c1790d7f0404b7e3bb469a47ee86b101a07898ac127ccd906ee718e4d1d'
