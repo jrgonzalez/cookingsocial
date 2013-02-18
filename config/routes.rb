57043886988fc7b7aa2db76f4743fdb2753fd2ba
rdutra <rdutra@timbasoftware.com>
2013-02-18 14:31:15 -0200

    added root action

--- config/routes.rb
+++ config/routes.rb
@@ -58,5 +58,5 @@ Cookingsocial::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
 
-  root :to => "home#index"
+  root :to => "application#index"
 end
